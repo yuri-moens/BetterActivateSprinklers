f6c9e91a7c69f162b416604740e9c5b8b609ace9
Jinks <jinks+git@mended-drum.net>
2020-02-23 17:00:17 +0100

    Update for SDV 1.4 compatibility
      - ModBuildConfig 2.2.0 -> 3.1.0
      - inlined Vanilla sprinkler activation

---
+++ GlobalSuppressions.cs
@@ -0,0 +1,9 @@
+﻿
+// This file is used by Code Analysis to maintain SuppressMessage 
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given 
+// a specific target and scoped to a namespace, type, member, etc.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0018:Inline variable declaration", Justification = "<Pending>", Scope = "member", Target = "~M:BetterActivateSprinklers.ModEntry.WaterTile(Microsoft.Xna.Framework.Vector2)")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Redundancies in Code", "RECS0065:Expression is always 'true' or always 'false'", Justification = "<Pending>", Scope = "member", Target = "~M:BetterActivateSprinklers.ModEntry.OnButtonPressed(System.Object,StardewModdingAPI.Events.ButtonPressedEventArgs)")]
+
